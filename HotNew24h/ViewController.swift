75595856bcab27e223daa79952d4887cb01a1c7c
ThanDucc <thanduc2k1@gmail.com>
2022-12-22 10:37:57 +0700

    Initial Commit

---
+++ HotNew24h/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotNew24h
+//
+//  Created by ThanDuc on 22/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
