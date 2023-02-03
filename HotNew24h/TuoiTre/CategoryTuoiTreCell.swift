4aeb8b33f722ff3ff36cb55e1b05db185b6e5a59
ThanDucc <thanduc2k1@gmail.com>
2023-02-03 21:36:28 +0700

    The First Commit

---
+++ HotNew24h/TuoiTre/CategoryTuoiTreCell.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryTuoiTreCell.swift
+//  HotNew24h
+//
+//  Created by ThanDuc on 29/12/2022.
+//
+
+import UIKit
+
+class CategoryTuoiTreCell: UICollectionViewCell {
+    
+    @IBOutlet weak var viewCell: UIView!
+    @IBOutlet weak var lbTittle: UILabel!
+    
+}
